3090ad63875621c0e5ff219aafebc1ee638cb850
Fernando Herrera <fernando.herrera85@gmail.com>
2021-04-11 11:04:14 -0400

    Inicio

--- lib/models/usuario.dart
+++ lib/models/usuario.dart
@@ -2,14 +2,14 @@
 
 class Usuario {
   
-  String nombre;
-  int edad;
+  String? nombre;
+  int? edad;
   List<String> profesiones;
 
   Usuario({
-    required this.nombre,
-    required this.edad,
-    required this.profesiones 
+    this.nombre,
+    this.edad,
+    this.profesiones = const []
   });
 
 }
